65e3aa94359f61b76cffbe8d188215063512d605
Ruslan <ruslan.salmin@gmail.com>
2024-10-03 15:16:09 +0300

    task4

---
+++ Копия/main.py
@@ -0,0 +1,49 @@
+import data_download as dd
+import data_plotting as dplt
+
+
+def main():
+    print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
+    print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
+    print("Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
+
+    # ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
+    # period = input("Введите период для данных (например, '1mo' для одного месяца): ")
+    # while True:
+    #     threshold = input("Введите порог допустимых колебаний цен: ")
+    #     try:
+    #         threshold = float(threshold)
+    #         break
+    #     except:
+    #         print("Порог должен быть числом")
+    ticker = 'AAPL'
+    period = '5d'
+    threshold = 20
+
+
+    # Fetch stock data
+    stock_data = dd.fetch_stock_data(ticker, period)
+
+    # Add moving average to the data
+    stock_data = dd.add_moving_average(stock_data)
+
+    # Plot the data
+    # dplt.create_and_save_plot(stock_data, ticker, period)
+
+    #average close
+    dd.calculate_and_display_average_price(stock_data)
+
+    #price fluctuation threshold
+    dd.notify_if_strong_fluctuations(stock_data, threshold)
+
+    # save to .csv
+    dd.export_data_to_csv(stock_data, "data")
+
+    # расчет индикаторов торгов
+    data_with_indicator = dd.add_calculate_indicator(stock_data)
+    # вывод цен с индикаторами
+    dplt.show_data_with_indicator(data_with_indicator, ticker)
+
+
+if __name__ == "__main__":
+    main()
